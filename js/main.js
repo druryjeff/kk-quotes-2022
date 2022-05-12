072424d5c08502c8d964bba7192df8115813058d
Jeff Drury <me@jeffdrury.com>
2022-05-12 11:59:31 -0400

    Tweak button iconography

--- js/main.js
+++ js/main.js
@@ -220,7 +220,7 @@ function checkFavs(){
 function showProcessing(){
 	// vars
 	const span = btnSaveFav.querySelector('span');
-	const defaultText = span.textContent;
+	const defaultIcon = span.innerHTML;
 	const spacebarNote = document.querySelector('footer p');;
 	const progressCircle = '<svg class="icon-progress" viewBox="0 0 512 512" title="circle-notch"><path d="M288 39.056v16.659c0 10.804 7.281 20.159 17.686 23.066C383.204 100.434 440 171.518 440 256c0 101.689-82.295 184-184 184-101.689 0-184-82.295-184-184 0-84.47 56.786-155.564 134.312-177.219C216.719 75.874 224 66.517 224 55.712V39.064c0-15.709-14.834-27.153-30.046-23.234C86.603 43.482 7.394 141.206 8.003 257.332c.72 137.052 111.477 246.956 248.531 246.667C393.255 503.711 504 392.788 504 256c0-115.633-79.14-212.779-186.211-240.236C302.678 11.889 288 23.456 288 39.056z" /></svg>';
 	const progressCheck = '<svg class="icon-check" viewBox="0 0 512 512" title="check"><path d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z" /></svg>';
@@ -238,7 +238,7 @@ function showProcessing(){
 	  saveFavs();
 	  // processing final stage
 	  setTimeout(function(){
-	    span.innerHTML = defaultText;
+	    span.innerHTML = defaultIcon;
 	    btnSaveFav.classList.remove('confirmed','saving');
 	    btnOpenFav.disabled = false;
 	    window.addEventListener('keypress', displayRandomQuote);

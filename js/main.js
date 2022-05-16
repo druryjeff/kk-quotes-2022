ef3c3be0986c2ce716a40c80db31a602a69cf865
Jeff Drury <me@jeffdrury.com>
2022-05-16 08:40:41 -0400

    Update UI details

--- js/main.js
+++ js/main.js
@@ -138,8 +138,9 @@ function getFavs(){
 	const favsGroup = document.querySelector('.favs-group');
 	if( localStorage.getItem('favs') ){
 		const favsResults = JSON.parse(localStorage.getItem('favs'));
+		const removeQuote = '<svg viewBox="0 0 448 512" title="trash-alt"><path d="M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z" /></svg>';
 		const favs = favsResults.map(function(f,i){
-			return `<div class="fav-quote"><blockquote><p data-index="${i}">${f}</p></blockquote><button>X</button></div>`;
+			return `<div class="fav-quote"><blockquote><p data-index="${i}">${f}</p></blockquote><button>${removeQuote}</button></div>`;
 		})
 		favsGroup.innerHTML = favs.reverse().join('');
 		// handle delete
